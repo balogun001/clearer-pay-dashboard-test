250c6d9e5d3b9fc855f1c396fbed82daefde93af
balogun001 <balogunmoyinoluwav@gmail.com>
2025-05-15 16:29:16 +0100

    delete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import { Button } from '@ui/Button';
-import { Checkbox } from '@ui/Checkbox';
-import { ImageInput } from '@ui/ImageInput';
-import { Input } from '@ui/Input';
-import { PasswordInput } from '@ui/PasswordInput';
-import { Select } from '@ui/Select';
 import { BodyText, Title } from '@ui/Text';
-import { TextArea } from '@ui/Textarea';
 import { Wrapper } from '@ui/Wrapper';
 
 export default function Home() {
@@ -16,12 +10,6 @@ export default function Home() {
         <Title>Nextjs App</Title>
         <Button>test</Button>
         <Button kinds="secondary">test2</Button>
-        <Input label="test" placeholder="test" />
-        <PasswordInput label="test" placeholder="test" />
-        <TextArea label="test" placeholder="test" />
-        <Checkbox />
-        <Select label="text" />
-        <ImageInput onChange={() => console.log('test Image Input')} />
       </main>
     </Wrapper>
   );
